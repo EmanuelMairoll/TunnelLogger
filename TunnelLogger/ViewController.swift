36257c5365e1d676804b893115ef002e7ad23126
Zeldinator <accounts@emanuelmairoll.at>
2018-07-17 17:08:14 +0200

    Initial Commit

---
+++ TunnelLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TunnelLogger
+//
+//  Created by Emanuel Mairoll on 17.07.18.
+//  Copyright © 2018 Emanuel Mairoll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
